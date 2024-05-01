a9f1f746dc8966dacc7eca59c899a17028dd5094
Jhon Yaguana <jyaguana3@utmachala.edu.ec>
2024-04-30 23:35:32 -0500

    conection

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express =  require ('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = mysql.createConnection({
+    host: 'roundhouse.proxy.rlwy.net',
+    user: 'root',
+    password: 'vadMiLhwtXmOWeTpYchfRPDHPzeZJqhr',
+    database: 'railway',
+  });
+  
+  db.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL:', err);
+    } else {
+      console.log('Connected to MySQL');
+    }
+  });
 
 
 app.get('/', (req, res) => {
