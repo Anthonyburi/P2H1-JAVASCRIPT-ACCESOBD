3c7e0c468accb615e0ff67175b3bc5e9477edd08
YaguanaJhonJY <87989324+YaguanaJhonJY@users.noreply.github.com>
2024-05-01 16:05:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/home', obtenerUsuarios, (req, res) => {
   res.sendFile(__dirname + '/home.html');
 });
 
-  app.get('/acercade', (req, res) => {
+  app.get('/home/acercade', (req, res) => {
     res.sendFile(__dirname + '/acerca.html');
   });
   app.get('/home/servicios', (req, res) => {
